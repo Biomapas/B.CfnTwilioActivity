4e870e6dc81562c026cd1b833b9423174ce32275
laimonassutkus <laimonas.sutkus@gmail.com>
2020-10-30 14:11:42 +0200

    Initial.

---
+++ b_cfn_twilio_activity_test/testing_infrastructure.py
@@ -0,0 +1,26 @@
+from aws_cdk.core import Stack
+
+from b_cfn_twilio_activity.function import TwilioActivitySingletonFunction
+from b_cfn_twilio_activity.resource import TwilioActivityResource
+
+
+class TestingInfrastructure(Stack):
+    def __init__(self, scope: Stack):
+        super().__init__(
+            scope=scope,
+            id=f'TestingStack',
+            stack_name=f'TestingStack'
+        )
+
+        function = TwilioActivitySingletonFunction(
+            scope=self,
+            name='TestingFunction',
+            twilio_account_sid='Test1',
+            twilio_auth_token='Test2',
+            twilio_workspace_sid='Test3'
+        )
+
+        TwilioActivityResource(
+            scope=self,
+            activity_function=function
+        )
